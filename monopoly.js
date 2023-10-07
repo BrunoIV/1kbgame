7b14b97b445f4152b65c8166189f7056f343f7b0
BrunoIV <36341312+BrunoIV@users.noreply.github.com>
2023-10-07 17:32:19 +0200

    Simplifico el cálculo de la desviación de los caracteres + agrego varias casillas

--- monopoly.js
+++ monopoly.js
@@ -6,19 +6,19 @@ colors = 'ea43f5ded000',
 text = (txt, color, x, y, size) => {
     ctx.fillStyle = "#" + color;
     ctx.font = size + "px Monospace";
-    ctx.textBaseline = "top"; //alphabetic by default
-    ctx.textAlign = "left"; // Alinea el texto a la izquierda del punto de inicio
     ctx.fillText(txt, x, y); 
 },
 
-card=(size, color)=>{
-    var multiplier = (size/100);
-    text("━", colors.substring(color, color+3), 3, -31 * multiplier, size);
-    text("▯", "000", -(size/5), -(size/16.6), size);
-    text("🚆", "", 13*multiplier, 40*multiplier, size/4); //icon
+card=(size, color, index)=>{
+    var desviationX = index*size/1.9;
+    text("━", colors.substring(color, color+3), 5 + desviationX, size*.4, size*.9); //Color de la caja
+    text("▯", "000", -(size/5) + desviationX, size*.7, size); //caja
+    text("🚆", "", desviationX + size/8, size*.6, size/4); //icono
 };
 
-card(100, 2);
+for(i=0; i<3;i++) {
+    card(100, i, i);
+}
 
 setInterval(_=>{
 }, 999);
